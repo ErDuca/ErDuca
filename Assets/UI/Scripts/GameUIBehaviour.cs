54b9851715a450df7fab6e51a4d18014ac8a8c35
Andrea Ricci <andrearicci500@gmail.com>
2022-03-22 17:43:27 +0100

    Reduce number of UI info boxes (2->1)

--- Assets/UI/Scripts/GameUIBehaviour.cs
+++ Assets/UI/Scripts/GameUIBehaviour.cs
@@ -4,5 +4,33 @@ using UnityEngine;
 
 public class GameUIBehaviour : MonoBehaviour
 {
+    [SerializeField] private float infoTurnAnimTime;
+    [SerializeField] private GameObject infoImage;
 
+    //TODO: Replace int with proper GameObject type
+    public void PieceInfoTurn(int piece) => StartCoroutine(PieceInfoTurnCoroutine(piece));
+
+    IEnumerator PieceInfoTurnCoroutine(int piece)
+    {
+        float elapsedTime = 0f;
+        while (elapsedTime < infoTurnAnimTime)
+        {
+            //TODO: get values from current scale
+            infoImage.transform.localScale = Vector3.Lerp(Vector3.one, new Vector3(0f, 1f, 1f), (elapsedTime / infoTurnAnimTime));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        infoImage.transform.localScale = Vector3.zero;
+        //TODO: add image change
+        elapsedTime = 0f;
+        while (elapsedTime < infoTurnAnimTime)
+        {
+            //TODO: get values from current scale
+            infoImage.transform.localScale = Vector3.Lerp(new Vector3(0f, 1f, 1f), Vector3.one, (elapsedTime / infoTurnAnimTime));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        infoImage.transform.localScale = Vector3.one;
+        yield return null;
+    }
 }
