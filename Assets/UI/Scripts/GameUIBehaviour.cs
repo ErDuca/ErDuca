e5e7fd24ee52bd6403658c66077b22634fcfc404
Andrea Ricci <andrearicci500@gmail.com>
2022-03-26 18:55:47 +0100

    Update gameplay scene UI Prototype (updates to turn swap objects)

--- Assets/UI/Scripts/GameUIBehaviour.cs
+++ Assets/UI/Scripts/GameUIBehaviour.cs
@@ -4,29 +4,71 @@ using UnityEngine.UI;
 
 public class GameUIBehaviour : MonoBehaviour
 {
-    //For the "card turning" effect when examining piece info
-    [Header("Info card turning")]
+    [Header("Info card related")]
     [SerializeField] private float infoTurnAnimTime;
     [SerializeField] private GameObject infoImage;
-    //For the animations when changing turns
-    [Header("Turns")]
+    [SerializeField] private GameObject infoBlock;
+
+    [Header("Turns related")]
     [SerializeField] private GameObject thinkingIcon;
     [SerializeField] private GameObject playersTurnLogos;
     [SerializeField] private GameObject opponentsTurnLogos;
     [SerializeField] private AnimationClip turnSwapAnimation;
     [SerializeField] private Text turnText;
-    //For the timer objects
-    [Header("Timer")]
+    private Color colorBlue = new Color(0.35f, 0.41f, 0.96f);
+    private Color colorRed = new Color(0.96f, 0.41f, 0.35f);
+
+    [Header("Timer related")]
     [SerializeField] private Text timeText;
-    [SerializeField] private Slider timeSlider;
+    [SerializeField] private Slider timeSlider; 
+    private RectTransform timerSliderRectTransform; //used to get timeSlider's width to move the image on the other side
     [SerializeField] private GameObject timeSliderFill;
-    private float timeRemaining;
-    [SerializeField] private float turnTime;
+    [SerializeField] private GameObject timeSliderImage;
     private bool changingTurn;
+    private float timeRemaining;
+    private float timeToDisplay;
+    [SerializeField] private float turnTime;
+
+    [Header("Pause menu related")]
+    [SerializeField] private GameObject pauseMenu;
+    [SerializeField] private GameObject eventSystem;
+    [SerializeField] private Animator pauseAnimator;
+    [SerializeField] private AnimationClip pauseMenuOpenAnimation;
+    [SerializeField] private AnimationClip pauseMenuCloseAnimation;
+
+    public void PauseGame() => StartCoroutine(PauseGameCoroutine());
+    private IEnumerator PauseGameCoroutine()
+    {
+        eventSystem.SetActive(false);
+        pauseMenu.SetActive(true);
+        yield return new WaitForSeconds(pauseMenuOpenAnimation.length);
+        eventSystem.SetActive(true);
+    }
+
+    public void ResumeGame() => StartCoroutine(ResumeGameCoroutine());
+    private IEnumerator ResumeGameCoroutine()
+    {
+        eventSystem.SetActive(false);
+        pauseAnimator.SetTrigger("closePauseMenu");
+        yield return new WaitForSeconds(pauseMenuCloseAnimation.length);
+        pauseAnimator.SetTrigger("closePauseMenu");
+        pauseMenu.SetActive(false);
+        eventSystem.SetActive(true);
+    }
+
+    //TODO: Replace int with proper GameObject type
+    public void ShowPieceInfo(int piece)
+    {
+        infoBlock.SetActive(true);
+    }
+    public void HidePieceInfo()
+    {
+        infoBlock.SetActive(false);
+    }
 
     //TODO: Replace int with proper GameObject type
     public void PieceInfoTurn(int piece) => StartCoroutine(PieceInfoTurnCoroutine(piece));
-    IEnumerator PieceInfoTurnCoroutine(int piece)
+    private IEnumerator PieceInfoTurnCoroutine(int piece)
     {
         float elapsedTime = 0f;
         while (elapsedTime < infoTurnAnimTime)
@@ -52,27 +94,45 @@ public class GameUIBehaviour : MonoBehaviour
     }
 
     public void PlayersTurnStart() => StartCoroutine(PlayersTurnStartCoroutine());
-    IEnumerator PlayersTurnStartCoroutine()
+    private IEnumerator PlayersTurnStartCoroutine()
     {
+        //changingturn stops time during turn swap animations
         changingTurn = true;
         thinkingIcon.SetActive(false);
         playersTurnLogos.SetActive(true);
+        //animation starts immediately when enabled
         yield return new WaitForSeconds(turnSwapAnimation.length);
+
+        //Everything tunr-related gets moved on the left side
+        turnText.alignment = TextAnchor.MiddleLeft;
         turnText.text = "PLAYER'S\nTURN";
-        timeSliderFill.GetComponent<Image>().color = new Color(0.35f, 0.41f, 0.96f);
+        timeSlider.direction = Slider.Direction.LeftToRight;
+        timeSliderFill.GetComponent<Image>().color = colorBlue;
+        //TODO: The image bugs out (the position is resolution dependent)
+        timeSliderImage.transform.position = new Vector3(timeSlider.transform.position.x - timerSliderRectTransform.rect.width / 2, timeSlider.transform.position.y, timeSlider.transform.position.z);
+
         playersTurnLogos.SetActive(false);
         timeRemaining = turnTime;
         changingTurn = false;
     }
 
     public void OpponentsTurnStart() => StartCoroutine(OpponentsTurnStartCoroutine());
-    IEnumerator OpponentsTurnStartCoroutine()
+    private IEnumerator OpponentsTurnStartCoroutine()
     {
+        //changingturn stops turn timer during turn swap animations
         changingTurn = true;
         opponentsTurnLogos.SetActive(true);
+        //animation starts immediately when enabled
         yield return new WaitForSeconds(turnSwapAnimation.length);
+
+        //Everything tunr-related gets moved on the right side
+        turnText.alignment = TextAnchor.MiddleRight;
         turnText.text = "OPPONENT'S\nTURN";
-        timeSliderFill.GetComponent<Image>().color = new Color(0.96f, 0.41f, 0.35f);
+        timeSlider.direction = Slider.Direction.RightToLeft;
+        timeSliderFill.GetComponent<Image>().color = colorRed;
+        //TODO: The image bugs out (the position is resolution dependent)
+        timeSliderImage.transform.position = new Vector3(timeSlider.transform.position.x + timerSliderRectTransform.rect.width / 2, timeSlider.transform.position.y, timeSlider.transform.position.z);
+
         opponentsTurnLogos.SetActive(false);
         thinkingIcon.SetActive(true);
         timeRemaining = turnTime;
@@ -81,10 +141,18 @@ public class GameUIBehaviour : MonoBehaviour
 
     private void Start()
     {
+        //TODO: A mechanism to choose who starts (other scene?)
+
+        timerSliderRectTransform = (RectTransform)timeSlider.transform;
         turnText.text = "PLAYER'S\nTURN";
         timeText.text = "00:00";
         timeRemaining = turnTime;
         changingTurn = false;
+        pauseMenu.SetActive(false);
+        thinkingIcon.SetActive(false);
+        playersTurnLogos.SetActive(false);
+        opponentsTurnLogos.SetActive(false);
+        infoBlock.SetActive(false);
     }
 
     private void Update()
@@ -94,10 +162,12 @@ public class GameUIBehaviour : MonoBehaviour
             if(!changingTurn)
             {
                 timeRemaining -= Time.deltaTime;
-                timeText.text = "00:" + (int)timeRemaining;
+                //We add 1 because it's more intuitive (if I set 70 secs it starts as 01:10 and not 01:09; time ends as soon as displaying 0)
+                timeToDisplay = timeRemaining + 1;
+                //Sets time text to minutes:seconds, both always displayed as 2 digits
+                timeText.text = string.Format("{0:00}:{1:00}", Mathf.FloorToInt(timeToDisplay / 60), Mathf.FloorToInt(timeToDisplay % 60));
                 timeSlider.value = timeRemaining / turnTime;
-            }
-            
+            } 
         }
         else
         {
