454922bfdc50b6139a5fa43fd71f5e938570628c
StefanoCld <caldana.stefano@hotmail.it>
2022-04-21 15:34:29 +0200

    Modified ErDucaPlayer

--- Assets/Scripts/Network/ErDucaPiece.cs
+++ Assets/Scripts/Network/ErDucaPiece.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
 
-public class ErDucaPiece : MonoBehaviour
+public class ErDucaPiece : NetworkBehaviour
 {
     // Start is called before the first frame update
     void Start()
