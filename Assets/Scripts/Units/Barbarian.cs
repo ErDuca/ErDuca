f9676ac22aff2e8e137d6913d03f6b8d017a5da2
StefanoCld <caldana.stefano@hotmail.it>
2022-05-12 14:28:51 +0200

    Minor fix

--- Assets/Scripts/Units/Barbarian.cs
+++ Assets/Scripts/Units/Barbarian.cs
@@ -12,26 +12,17 @@ public class Barbarian : ErDucaPiece
     }
     void Start()
     {
-        //Command not implementable -> Generale is about to be deprecated!
-        /*
-        mPhaseOneMovementArray.Add(new Movement(0, -1, Ptype.Move));
-        mPhaseOneMovementArray.Add(new Movement(2, 0, Ptype.Move));
-        mPhaseOneMovementArray.Add(new Movement(0, -1, Ptype.Move));
-        mPhaseOneMovementArray.Add(new Movement(-2, 0, Ptype.Move));
-        mPhaseOneMovementArray.Add(new Movement(-2, -1, Ptype.Jump));
-        mPhaseOneMovementArray.Add(new Movement(-2, 1, Ptype.Jump));
+        _PhaseOneMovementArray.Add(new Movement(0, -1, Ptype.Walk));
+        _PhaseOneMovementArray.Add(new Movement(2, 0, Ptype.Walk));
+        _PhaseOneMovementArray.Add(new Movement(0, -1, Ptype.Walk));
+        _PhaseOneMovementArray.Add(new Movement(-2, 0, Ptype.Walk));
+        _PhaseOneMovementArray.Add(new Movement(-2, -1, Ptype.Jump));
+        _PhaseOneMovementArray.Add(new Movement(-2, 1, Ptype.Jump));
 
-        mPhaseOneMovementArray.Add(new Movement(0, -1, Ptype.Move));
-        mPhaseOneMovementArray.Add(new Movement(1, 0, Ptype.Move));
-        mPhaseOneMovementArray.Add(new Movement(2, 0, Ptype.Move));
-        mPhaseOneMovementArray.Add(new Movement(-1, 0, Ptype.Move));
-        mPhaseOneMovementArray.Add(new Movement(-2, 0, Ptype.Move));
-
-        mPhaseOneMovementArray.Add(new Movement(1, -1, Ptype.Command));
-        mPhaseOneMovementArray.Add(new Movement(1, -1, Ptype.Command));
-        mPhaseOneMovementArray.Add(new Movement(1, 0, Ptype.Command));
-        mPhaseOneMovementArray.Add(new Movement(1, 1, Ptype.Command));
-        */
-        // Mancano 2 command che si sovrappongono
+        _PhaseOneMovementArray.Add(new Movement(0, -1, Ptype.Walk));
+        _PhaseOneMovementArray.Add(new Movement(1, 0, Ptype.Walk));
+        _PhaseOneMovementArray.Add(new Movement(2, 0, Ptype.Walk));
+        _PhaseOneMovementArray.Add(new Movement(-1, 0, Ptype.Walk));
+        _PhaseOneMovementArray.Add(new Movement(-2, 0, Ptype.Walk));
     }
 }
